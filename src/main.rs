3cd6819d875a1e42cd57e3ee6a87ee8a22825b78
Henry Baldwin <henrymbaldwin@gmail.com>
2024-05-29 17:12:05 -0400

    Push

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use macroquad::prelude::*;
+use rand::Rng;
+struct State {
+    board: [[u16; 4]; 4],
+    score: u32,
+}
+
+impl State {
+    fn new() -> Self {
+        
+    }
+
+
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
     loop {
-        clear_background(RED);
+        clear_background(WHITE);
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
