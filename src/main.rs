21c6d362822c890010aac2a4c7f971b446b117d4
Henry Baldwin <henrymbaldwin@gmail.com>
2024-05-29 17:03:50 -0400

    ggez more like gghard. Switching to macroquad.

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,16 @@
-use ggez::*;
-use rand::Rng;
+use macroquad::prelude::*;
 
-struct State {
-    //4x4 array of u16
-    board: [[u16; 4]; 4],
-    score: u32,
-}
-
-impl ggez::event::EventHandler<GameError> for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-
-        Ok(())
-    }
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        
-        
-        Ok(())
-    }
-}
-
-fn gen_tile() -> u16 {
-    //generate a 2 or 4 with 90% and 10% probability respectively
-    let mut rng = rand::thread_rng();
-    let x = rng.gen_range(0..10);
-    if x == 0 {
-        return 4;
-    } else {
-        return 2;
-    }
-}
-
-fn main() {
-    let mut rng = rand::thread_rng();
-    
-    let mut state = State {
-        board: [[0; 4]; 4],
-        score: 0,
-    };
-
-    //generate initial two tiles            
-    let x0 = rng.gen_range(0..4);
-    let y0 = rng.gen_range(0..4);
-
-    let mut x1;
-    let mut y1;
+#[macroquad::main("BasicShapes")]
+async fn main() {
     loop {
-        x1 = rng.gen_range(0..4);
-        y1 = rng.gen_range(0..4);
+        clear_background(RED);
 
-        if x0 != x1 && y0 != y1 {
-            break;
-        }   
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
     }
-
-    state.board[x0][y0] = gen_tile();
-    state.board[x1][y1] = gen_tile();
-
-    let c = conf::Conf::new();
-    let (ctx, event_loop) = ContextBuilder::new("2048", "Henry Baldwin")
-        .default_conf(c)
-        .build()
-        .unwrap();
-
-    event::run(ctx, event_loop, state);
 }
